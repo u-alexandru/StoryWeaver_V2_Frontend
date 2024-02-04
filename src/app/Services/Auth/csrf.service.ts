4e8161c9279b8b10c36bbeea88e7b4e6478075e8
Alexandru Ungureanu <u95.alexandru@gmail.com>
2024-02-05 00:19:00 +0100

    New cookie service

--- src/app/Services/Auth/csrf.service.ts
+++ src/app/Services/Auth/csrf.service.ts
@@ -1,26 +1,20 @@
 import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import { DOCUMENT } from '@angular/common';
+import {SsrCookieService} from "ngx-cookie-service-ssr";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CsrfService {
 
-  constructor(private http: HttpClient, @Inject(DOCUMENT) private document: Document) { }
+  constructor(private http: HttpClient, private cookieService: SsrCookieService) { }
 
   getCsrfCookie() {
     return this.http.get('sanctum/csrf-cookie', {withCredentials: true});
   }
 
   getCsrfCookieFromCookies() {
-    const cookies = this.document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].split('=');
-      if (cookie[0].trim() === 'XSRF-TOKEN') {
-        return decodeURIComponent(cookie[1]);
-      }
-    }
-    return null;
+    return this.cookieService.check('XSRF-TOKEN') ? this.cookieService.get('XSRF-TOKEN') : null;
   }
 }
